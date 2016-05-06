1f122cafad9ce842ea8795b555d9e95598f14db5
fuzzwizard <momojak@gmail.com>
2016-05-06 01:30:09 -0700

    Minimalist design, no animation

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// import $ from 'jquery';
+//
+// $(window).ready(() => {
+//   $('.js-animation-hook').addClass('js-transition-in');
+// });
---
+++ src/js/core.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$(window).ready(() => {
+  $('.animation-hook').addClass('transition-in');
+});
